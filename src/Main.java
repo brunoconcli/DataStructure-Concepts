09403610acd109070a792668c75ba67e14fb78f8
Bruno Concli <brunoconcli@gmail.com>
2023-08-28 17:14:10 -0300

    feat: CHanges to testings in main

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,28 @@
-import linkedList.LinkedListCircular;
-import linkedList.LinkedListUnordered;
+import linkedLists.LinkedListUnordered;
+import linkedLists.LinkedListDoubly;
+import linkedLists.LinkedListCircular;
 
 public class Main  {
     public static void main(String[] args) {
         try {
-            LinkedListCircular<String> uno = new LinkedListCircular<>();
+            LinkedListUnordered<String> uno = new LinkedListUnordered<>();
             LinkedListCircular<String> dos = new LinkedListCircular<>();
+            LinkedListDoubly<String> tres = new LinkedListDoubly<>();
 
             uno.addFirst("A");
             uno.addLast("B");
             uno.addLast("C");
-            uno.addLast("D");
-
+            System.out.println(uno.toString());
             dos.addFirst("A");
             dos.addLast("B");
             dos.addLast("C");
+
+            tres.addFirst("X");
+            tres.addLast("Z");
+            tres.addAfter(0, "Y");
+            tres.addFirst("W");
+            tres.addAfter(3, "A");
+            System.out.println(tres.toString());
 
         }
         catch(Exception e) {
