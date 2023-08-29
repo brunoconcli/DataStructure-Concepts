406d99d18434e3d7a6dda32377ac31d1ecb47905
Bruno <brunoconcli@gmail.com>
2023-08-28 22:42:11 -0300

    feat: Create class LinkedListOrdered

--- src/linkedLists/LinkedListOrdered.java
+++ src/linkedLists/LinkedListOrdered.java
@@ -1,7 +1,32 @@
 package linkedLists;
 
-public class LinkedListOrdered<X> implements ILinkedList {
-    public void addFirst(X info) {}
+public class LinkedListOrdered<X extends Comparable<X>> implements ILinkedList<X>, Cloneable {
+
+    private Node<X> first, last;
+    private int size = 0;
+
+    public void add(X info) throws Exception {
+        if (info == null) throw new Exception("Info passed must not be null");
+
+        if (this.first == null) {
+            this.first = new Node<X>(info, null);
+            this.last = this.first;
+            return;
+        }
+
+        // compareTo() < 0 --> this < obj
+        // compareTo() > 0 --> this > obj 
+        
+        // for (Node<X> current = this.first; current != null; current = current.getNext()) {
+        //     if (current.getInfo().compareTo(info) )
+        // }
+    }
+
+    @Override
+    public int getSize() {
+        return this.size;
+    }
+
     @Override
     public boolean isEmpty() {
         return false;
@@ -15,5 +40,20 @@ public class LinkedListOrdered<X> implements ILinkedList {
     @Override
     public void removeAllElements() {
 
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder message = new StringBuilder();
+
+        try {
+            for (Node<X> current = this.first; current != null; current = current.getNext()) {
+                message.append(current.getInfo());
+                if (current.getNext() != null) message.append(", ");
+            }
+        }
+        catch (Exception ignored) {}
+
+        return message.toString();
     }
 }
