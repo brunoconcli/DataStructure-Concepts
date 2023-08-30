122af108d50a0444478869f0f82aff727e5518f8
Bruno <brunoconcli@gmail.com>
2023-08-29 21:59:33 -0300

    feat: Added `clone()` and copy constructor

--- src/linkedLists/LinkedListOrdered.java
+++ src/linkedLists/LinkedListOrdered.java
@@ -5,6 +5,16 @@ public class LinkedListOrdered<X extends Comparable<X>> implements ILinkedList<X
     private Node<X> first, last;
     private int size = 0;
 
+    public LinkedListOrdered() {}
+
+    protected LinkedListOrdered(LinkedListOrdered<X> model) throws Exception {
+        if (model == null) throw new Exception ("Model object passed must not be null");
+        this.first = model.first;
+        this.last = model.last;
+        this.size = model.size;
+
+    }
+
     public void add(X info) throws Exception {
         if (info == null) throw new Exception("Info passed must not be null");
 
@@ -56,4 +66,18 @@ public class LinkedListOrdered<X extends Comparable<X>> implements ILinkedList<X
 
         return message.toString();
     }
+
+    @Override
+    public Object clone() {
+        LinkedListOrdered<X> ret = null;
+        try {
+            Object clone = super.clone();
+            ret = new LinkedListOrdered<>(this);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return ret;
+    }
+
 }
