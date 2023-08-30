b6fb65235b715d14d76eeac593f1cf94352d0976
Bruno <brunoconcli@gmail.com>
2023-08-29 21:50:44 -0300

    continue: Commented test

--- src/tests/linkedLists/LinkedListUnorderedTest.java
+++ src/tests/linkedLists/LinkedListUnorderedTest.java
@@ -1,11 +1,11 @@
-package tests.linkedLists;
-
-import org.junit.Test;
-
-class LinkedListUnorderedTest {
-    @Test
-    void allRemovingMethods() {
-    }
-}
-
-// continuar testagem
+//package tests.linkedLists;
+//
+//import org.junit.Test;
+//
+//class LinkedListUnorderedTest {
+//    @Test
+//    void allRemovingMethods() {
+//    }
+//}
+//
+//// continuar testagem
