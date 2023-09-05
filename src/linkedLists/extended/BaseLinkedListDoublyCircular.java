7416547643b0ff4e60fb9911f06e91784bcfe78c
Bruno <brunoconcli@gmail.com>
2023-09-05 20:42:01 -0300

    ref: Removing independent class `Node` and adding it to `Base Doubly Circular List`

--- src/linkedLists/extended/BaseLinkedListDoublyCircular.java
+++ src/linkedLists/extended/BaseLinkedListDoublyCircular.java
@@ -1,10 +1,100 @@
 package linkedLists.extended;
 
 import linkedLists.ILinkedList;
-import linkedLists.Node;
 
 public class BaseLinkedListDoublyCircular<X> implements ILinkedList<X>, Cloneable {
     
+	 
+	
+	protected class Node <N>{
+		private N info;
+		private Node <N> next, prev;
+		public Node (N info) {
+			this.info = info;
+			this.next = null;
+			this.prev = null;
+		}
+		public Node (Node<N> prev, N info) {
+			this.prev = prev;
+			this.info = info;
+			this.next = null;
+		}
+		public Node (N info, Node <N> next) {
+			this.prev = null;
+			this.info = info;
+			this.next = next;
+		}
+		public Node (Node<N> prev, N info, Node<N> next) {
+			this.prev = prev;
+			this.info = info;
+			this.next = next;
+		}
+		public N getInfo() throws Exception {
+			if (this.info == null)
+				throw new Exception("Node is empty");
+			return this.info;
+		}
+		public Node<N> getNext() {
+			return this.next;
+		}
+		public Node<N> getPrev() {
+			return this.prev;
+		}
+		public void setInfo(N info) {
+			this.info = info;
+		}
+		public void setNext(Node <N> next) {
+			this.next = next;
+		}
+		public void setPrev(Node<N> prev) {
+			this.prev = prev;
+		}
+		@Override
+		public String toString() {
+			String message = "";
+			try {
+				message += getInfo() + " -> " + null;
+				if (getNext() != null)
+					message = getInfo() + " -> " + getNext().getInfo();
+				if (getPrev() != null)
+					message = getPrev().getInfo() + " -> " + message;
+			}
+			catch (Exception e) { System.out.println(e.getMessage()); }
+
+
+			return message;
+		}
+		@Override
+		@SuppressWarnings("unchecked")
+		public boolean equals(Object obj) {
+			if (obj == null) return false;
+			if (this == obj) return true;
+
+			if (this.getClass() != obj.getClass()) return false;
+
+			Node<N> data = (Node<N>)obj;
+			if (this.prev != data.prev) return false;
+			if (this.info != data.info) return false;
+			if (this.next != data.next) return false;
+
+			return true;
+		}
+
+		@Override
+		public int hashCode() {
+			int hash = 2;
+			
+			hash = 3*hash + this.info.hashCode();	
+			if (this.next != null) hash = 3*hash + this.next.hashCode();
+			if (this.prev != null) hash = 3*hash + this.prev.hashCode();
+		
+			if (hash < 0) hash = -hash;
+			return hash;
+		}
+	}
+
+
+
 	protected Node<X> first, last;
 	protected int size;
 	public BaseLinkedListDoublyCircular() {
