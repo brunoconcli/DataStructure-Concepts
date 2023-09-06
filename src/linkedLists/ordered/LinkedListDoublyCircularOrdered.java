0882bb010eb58c15b180ec998921bdd1be528756
Bruno <brunoconcli@gmail.com>
2023-09-05 23:02:06 -0300

    feat: Create file `LinkedListDoublyCircularOrdered.java`

---
+++ src/linkedLists/ordered/LinkedListDoublyCircularOrdered.java
@@ -0,0 +1,6 @@
+package linkedLists.ordered;
+import linkedLists.extended.BaseLinkedListDoubly;
+
+public class LinkedListDoublyCircularOrdered<X extends Comparable<X>> extends BaseLinkedListDoubly<X> {
+    
+}
