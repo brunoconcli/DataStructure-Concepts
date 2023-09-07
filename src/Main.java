fc01160d895288bcd1df7c8868a1bbd7f34206df
Bruno <brunoconcli@gmail.com>
2023-09-07 16:32:46 -0300

    ref: Remove test code

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
-import linkedLists.ordered.LinkedListOrdered;
-
 public class Main  {
 	public static void main(String[] args) {
 		try {
-			LinkedListOrdered<String> uno = new LinkedListOrdered<String>();
-			
-			uno.add("C");
-			System.out.println(uno.toString());
-
-			uno.add("A");
-			System.out.println(uno.toString());
-
-			uno.add("B");
-			System.out.println(uno.toString());
+			System.out.println("Hello, World");
 		}
+		
 		catch(Exception e) {
 			System.out.println(e.getMessage());
 		}
