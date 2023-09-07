f68b1ff744587a912d638f3a41608b4817fb62f7
Bruno <brunoconcli@gmail.com>
2023-09-07 16:15:06 -0300

    feat: Implement `add` method

--- src/linkedLists/ordered/LinkedListCircularOrdered.java
+++ src/linkedLists/ordered/LinkedListCircularOrdered.java
@@ -2,5 +2,46 @@ package linkedLists.ordered;
 import linkedLists.extended.BaseLinkedListCircular;
 
 public class LinkedListCircularOrdered<X extends Comparable<X>> extends BaseLinkedListCircular<X> {
-    
+	public void add(X info) throws Exception {
+		if (info == null) throw new Exception ("Information passed must not be null");
+
+		if (this.first == null) {
+			this.first = this.last = new Node<X>(info, this.first);
+			
+			this.size++;
+			return;
+		}
+		
+		if (this.first.getInfo().compareTo(info) > 0) {
+			this.first = new Node<X>(info, this.first);
+			if (this.size == 1)
+				this.last = this.first.getNext();
+				
+			this.last.setNext(this.first);
+			this.size++;
+			return;
+		}
+
+		if (this.size == 1) {
+			this.last = new Node<X>(info, this.first);
+			this.first.setNext(this.last);
+
+			this.size++;
+			return;
+		}
+
+		Node<X> current = this.first.getNext(), previous = this.first;
+		for (int i = 0; i < this.size; i++) {
+			if (current.getInfo().compareTo(info) > 0) {
+				previous.setNext(new Node<X>(info, current));
+				this.size++;
+				return;
+			}
+			previous = current;
+			current = current.getNext();	
+		}
+		this.last.setNext(new Node<X>(info, this.first));
+		this.last = this.last.getNext();
+		this.size++;
+	}	
 }
